97421fba50ec812170863573711052beb44661e6
sbstn.vsqz <sebastian.vsqz0@gmail.com>
2023-12-21 00:17:43 -0300

    Prueba1

---
+++ evaluate.py
@@ -0,0 +1,22 @@
+import torch
+from tqdm import tqdm
+def evaluate(net,dataloader,device,criterion,n_val):
+    net.eval()
+    num_val_batches=len(dataloader)
+    loss = 0
+    acc = 0
+    with torch.no_grad():
+        for batch in tqdm(dataloader, total=num_val_batches, desc = "Validation", unit = "batch",leave=False):
+            img = batch["image"].float().to(device=device)
+            label = batch["label"].type(torch.uint8).to(device=device)
+
+            pred = net(img)
+            
+            label_pred = torch.argmax(pred,dim=1).float()
+
+            loss = criterion(pred,label).item()
+            acc += (label_pred==label).sum()
+        epoch_loss = loss/num_val_batches
+        epoch_acc = acc.item()/n_val
+
+    return epoch_loss, epoch_acc
